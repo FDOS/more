a1b7f17c473c81f3ee8996019fc27cc61706bd3e
Andrew Bird <ajb@spheresystems.co.uk>
2021-02-05 19:14:19 +0000

    Build: Move compiler options to batch file
    Notes:
      1/ for some reason there are extra source files here that aren't
      required for the build of more. For now just remove them from the
      build as someone may figure out their intended use.

---
+++ build.bat
@@ -0,0 +1,35 @@
+
+goto %1
+
+:watcom
+rem ############# WATCOM ########################
+set PATH=C:\bin;C:\watcom\binw;%PATH%
+rem # -we treat all warnings as errors
+rem # -wx set warning level to max
+rem # -zq operate quietly
+rem # -fm[=map_file]  generate map file
+rem # -fe=executable name executable file
+set WATCOM=C:\watcom
+set INCLUDE=C:\watcom\h
+set CC=wcl
+set COMFLAGS=-mt -lt
+set EXEFLAGS=-mc
+set CFLAGS=-bt=DOS -bcl=DOS -D__MSDOS__ -oas -s -wx -we -zq -fm %EXEFLAGS%-fe=
+set TARGET=more.exe
+goto doit
+
+:tcc
+rem ############# TURBO_C ########################
+set PATH=C:\bin;C:\tc\bin;%PATH%
+set CC=tcc
+set COMFLAGS=-mt -lt -Z -O -k-
+set EXEFLAGS=-mc -N -Z -O -k-
+set CFLAGS=-w -M -Z -O -k- %EXEFLAGS% -e
+rem tcc looks for includes from the current directory, not the location of the
+rem file that's trying to include them, so add kitten's location
+set CFLAGS=-I../kitten %CFLAGS%
+set TARGET=more.exe
+goto doit
+
+:doit
+make -C src %TARGET%
