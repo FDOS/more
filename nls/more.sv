5d50d4f6c0217a808c9552c48d23e07340c94a1e
Andrew Bird <ajb@spheresystems.co.uk>
2021-02-05 12:23:30 +0000

    Repository: Add .gitattributes and renomalise

--- nls/more.sv
+++ nls/more.sv
@@ -1,17 +1,17 @@
-# Translation by Martin Str봫berg <ams@ludd.luth.se>.
-0.0:Visa inneh냡let i en textfil en sk꼛m i taget
-0.1:Anv꼗dning
+# Translation by Martin Str철mberg <ams@ludd.luth.se>.
+0.0:Visa inneh책llet i en textfil en sk채rm i taget
+0.1:Anv채ndning
 0.2:kommando
 0.3:fil
-0.4:Tillg꼗gliga tangenter
+0.4:Tillg채ngliga tangenter
 0.5:Nn
-0.6:N꼜ta fil
+0.6:N채sta fil
 0.7:Qq
 0.8:Avsluta program
 0.9:Mellanslag
-0.10:N꼜ta sida
-1.0:Ok꼗d option
-1.1:Ingen s냗an fil
-1.2:Kan inte 봯pna filen
+0.10:N채sta sida
+1.0:Ok채nd option
+1.1:Ingen s책dan fil
+1.2:Kan inte 철ppna filen
 2.0:Mera
 2.1:<STDIN>
